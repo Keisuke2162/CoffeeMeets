d6f9cee69b91fd8f5c106d287ef9323e98e42529
Keisuke2162 <applabo.kei@gmail.com>
2024-05-21 02:14:04 +0900

    Initial Commit

---
+++ CoffeeMeets/CoffeeMeetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMeetsApp.swift
+//  CoffeeMeets
+//
+//  Created by Kei on 2024/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMeetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMeetsUITests/CoffeeMeetsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeMeetsUITestsLaunchTests.swift
+//  CoffeeMeetsUITests
+//
+//  Created by Kei on 2024/05/21.
+//
+
+import XCTest
+
+final class CoffeeMeetsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
